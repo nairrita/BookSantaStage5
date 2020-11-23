14dc7bfe7ea37d9294973221e9dc1804971717b6
NairritaMohan <nairrita.whitehatjr@gmail.com>
2020-11-24 00:20:08 +0530

    stage2

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {View,Text,StyleSheet,TextInput,TouchableOpacity,Alert} from 'react-native';
+import {View,Text,StyleSheet,TextInput,TouchableOpacity,Alert,ScrollView,Modal,KeyboardAvoidingView} from 'react-native';
 import db from '../config'
 import firebase from 'firebase'
 import SantaAnimation from '../components/SantaAnimation'
@@ -9,13 +9,29 @@ export default class WelcomeScreen extends React.Component{
         super()
         this.state = {
             email: ' ',
-            password: ' '
+            password: ' ',
+            isModalVisible : false,
+            firstName : ' ',
+            lastName : ' ',
+            address : ' ',
+            contact : ' ',
+            confirmPassword : ' '
         }
     }
 
-userSignUp = (email,password)=>{
+userSignUp = (email,password,confirmPassword)=>{
+    if(password !== confirmPassword){
+        return Alert.alert("password doesnt match")
+    }else{
 firebase.auth().createUserWithEmailAndPassword(email,password)
-.then(response=>{
+.then(()=>{
+    db.collection('user').add({
+        first_name : this.state.firstName,
+        last_name : this.state.lastName,
+        contact : this.state.contact,
+        email: this.state.email,
+        address: this.state.address
+    })
     return Alert.alert("User Added Succesfully")
 })
 .catch(error=>{
@@ -23,10 +39,12 @@ firebase.auth().createUserWithEmailAndPassword(email,password)
     var errorMessage = error.message
     return Alert.alert(errorMessage)
 })
+
+    }
 }
 userlogIn=(email,password)=>{
 firebase.auth().signInWithEmailAndPassword(email,password)
-.then(response=>{
+.then(()=>{
     return Alert.alert("Successfully LoggedIn")
 })
 .catch(error=>{
@@ -35,9 +53,125 @@ firebase.auth().signInWithEmailAndPassword(email,password)
     return Alert.alert(errorMessage)
 })
 }
+
+showModal = ()=>{
+    return(
+        <Modal 
+        animationType = 'fade'
+        transparent = {true}
+        visible = {this.state.isModalVisible}
+        >
+
+<View style = {styles.modalContainer}>
+<ScrollView style = {{width:'100%'}}>
+<KeyboardAvoidingView style = {styles.keyboardView}>
+<Text style = {styles.modalTitle}>
+Registration
+</Text>
+<TextInput 
+style = {styles.formBox}
+placeholder = {'FirstName'}
+maxLength = {8}
+onChangeText = {(text)=>{
+this.setState({
+    firstName:text
+})
+}}/>
+
+<TextInput 
+style = {styles.formBox}
+placeholder = {'LastName'}
+maxLength = {8}
+onChangeText = {(text)=>{
+this.setState({
+    lastName:text
+})
+}}/>
+
+<TextInput 
+style = {styles.formBox}
+placeholder = {'contact'}
+maxLength ={10}
+keyboardType = {'numeric'}
+onChangeText = {(text)=>{
+this.setState({
+    contact:text
+})
+}}/>
+
+<TextInput 
+style = {styles.formBox}
+placeholder = {'Address'}
+multiline = {true}
+onChangeText = {(text)=>{
+this.setState({
+    address:text
+})
+}}/>
+
+<TextInput 
+style = {styles.formBox}
+placeholder = {'Email'}
+keyboardType = "email-address"
+onChangeText = {(text)=>{
+this.setState({
+    email:text
+})
+}}/>
+
+<TextInput 
+style = {styles.formBox}
+placeholder = {'Password'}
+secureTextEntry = {true}
+onChangeText = {(text)=>{
+this.setState({
+    password:text
+})
+}}/>
+
+<TextInput 
+style = {styles.formBox}
+placeholder = {'ConfirmPassword'}
+secureTextEntry = {true}
+onChangeText = {(text)=>{
+this.setState({
+    confirmPassword:text
+})
+}}/>
+
+<View style= {styles.modalBackButton}>
+<TouchableOpacity style  = {styles.registerButton} 
+onPress = {()=>{
+    this.userSignUp(this.state.email,this.state.password,this.state.confirmPassword)
+    
+}}
+>
+<Text stylee= {styles.registerButtonText}>Register</Text>
+</TouchableOpacity>
+</View>
+<View style = {styles.modalBackButton}>
+<TouchableOpacity style = {styles.cancelButton} 
+onPress = {()=>{
+    this.setState({isModalVisible:false})
+}}
+>
+<Text style = {{color:'black'}}>CANCEL</Text>
+</TouchableOpacity>
+</View>
+</KeyboardAvoidingView>
+</ScrollView>
+</View>
+
+
+        </Modal>
+    )
+}
   render(){
     return(
       <View style = {styles.container}>
+          <View style = {{justifyContent:'center', alignItems:'center'}}>
+{this.showModal()}
+          </View>
           <View style = {styles.profileContainer}>
               <SantaAnimation/>
           <Text style = {styles.title}>BOOK SANTA</Text>
@@ -70,7 +204,8 @@ onPress ={()=>{
 </TouchableOpacity>
 <TouchableOpacity style = {styles.button} 
 onPress ={()=>{
-    this.userSignUp(this.state.email,this.state.password)
+   
+    this.setState({isModalVisible  : true})
 }}
 >
 <Text style = {styles.buttonText}>SignUp</Text>
@@ -131,5 +266,108 @@ const styles = StyleSheet.create({
       flex:1,
       justifyContent:'center',
       alignItems:'center'
-  }
+  },
+title :{
+    fontSize: 65,
+    fontWeight:'300',
+    paddingBottom:30,
+    color:'#ff3d00'
+},
+inputBox:{
+    width:300,
+    height:40,
+    borderBottomWidth:1.5,
+    borderColor: '#ff8a65',
+    fontSize:20,
+    margin:10,
+    paddingLeft : 10
+},
+keyboardView:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+},
+modalTitle:{
+    justifyContent:'center',
+    alignSelf : 'center',
+    fontSize:30,
+    color:'#ff5722',
+    margin:50
+},
+modalContainer:{
+    flex:1,
+    borderRadius:20,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#ffffff',
+    marginRight:30,
+    marginLeft:30,
+    marginTop:80,
+    marginBottom:80
+},
+formBox:{
+    width:'75%',
+    height:35,
+    alignSelf:'center',
+    borderColor:'#ffab91',
+    borderRadius:10,
+    borderWidth:1,
+    marginTop:20,
+    padding:10
+},
+registerButton:{
+width:200,
+height:40,
+alignSelf:'center',
+borderColor:'#ffab91',
+borderRadius:10,
+borderWidth:1,
+marginTop:20,
+padding:10
+},
+registerButton:{
+    width:200,
+    height:40,
+    alignItems:'center',
+    justifyContent:'center',
+    borderWidth:1,
+    borderRadius:10,
+    marginTop:30
+},
+registerButtonText:{
+    color:'#ff5722',
+    fontSize:15,
+    fontWeight:'bold'
+},
+cancelButton:{
+    width:200,
+    height:300,
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop:5,
+    borderColor:'black'
+},
+button:{
+    width:300,
+    height:50,
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:25,
+    backgroundColor:'#ff9800',
+    shadowColor : '#000',
+    shadowOffset:{
+        width:0,
+        height:8,
+    },
+    shadowOpacity : 0.30,
+    shadowRadius:10.32,
+    elevation:16,
+    padding:10
+},
+buttonText:{
+    color:'#ffff',
+    fontWeight:'200',
+    fontSize:20
+}
+
 })
